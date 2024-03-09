9a52714fe2fb477543efc164c08b6898fefa3c2a
Archana <archanadevisankar@gmail.com>
2024-03-09 08:39:54 +0530

    POST

--- src/main/java/org/example/Modules/PayloadManager.java
+++ src/main/java/org/example/Modules/PayloadManager.java
@@ -1,4 +1,27 @@
 package org.example.Modules;
 
+import com.google.gson.Gson;
+import org.example.payloads.Booking;
+import org.example.payloads.Bookingdates;
+
 public class PayloadManager {
+    //JAVA->JSON
+    public String CreatePayload()
+    {
+    Booking payload=new Booking();
+        payload.setFirstname("Jakie");
+        payload.setLastname("Brown");
+        payload.setTotalprice(1234);
+        payload.setDepositpaid(true);
+    Bookingdates bookingdates=new Bookingdates();
+        bookingdates.setCheckin("2018-01-01");
+        bookingdates.setCheckout("2019-01-01");
+        payload.setBookingdates(bookingdates);
+        payload.setAdditionalneeds("Breakfast");
+        System.out.println(payload);
+        Gson gson=new Gson();
+        String Jsonpayload=gson.toJson(payload);
+        System.out.println(Jsonpayload);
+        return Jsonpayload;
+}
 }
